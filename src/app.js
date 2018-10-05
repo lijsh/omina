e639aee06dee58f99116d86794eaced34cb2b095
Jason Lee <sysulijs@gmail.com>
2018-10-06 01:58:01 +0800

    typo

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ import { set, del, observe } from './observe/index'
 import mitt from 'mitt'
 
 export default function wxApp(config) {
-  const { onLaunch: originalOnLauch } = config
+  const { onLaunch: originalOnLaunch } = config
   config.onLaunch = function(...args) {
     promisify(this)
     this.globalData = this.globalData || {}
     observe(this.globalData)
-    if (originalOnLauch) originalOnLauch.bind(this)(...args)
+    if (originalOnLauch) originalOnLaunch.bind(this)(...args)
   }
 
   config.bus = mitt()
